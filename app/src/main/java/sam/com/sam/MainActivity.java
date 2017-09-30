1f2a2f3377caa62caa7e855c6c7ec6162b2b886a
ZSte <uzecm@student.kit.edu>
2017-09-30 16:37:05 +0200

    Signout

--- app/src/main/java/sam/com/sam/MainActivity.java
+++ app/src/main/java/sam/com/sam/MainActivity.java
@@ -1,8 +1,11 @@
 package sam.com.sam;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
@@ -12,7 +15,7 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private FirebaseDatabase firebaseDatabase;
     private DatabaseReference databaseReference;
@@ -25,6 +28,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference().child("users");
+
+        //Button signOut = (Button) findViewById(R.id.signout);
+        //signOut.setOnClickListener(this);
 
         firebaseAuth = FirebaseAuth.getInstance();
         authStateListener = new FirebaseAuth.AuthStateListener() {
@@ -53,9 +62,38 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                //User is succesfully signed in
+                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                User u = new User(firebaseUser.getDisplayName(), -1, firebaseUser.getEmail(), null, null);
+                databaseReference.push().setValue(u);
+            } else if (resultCode == 0) {
+                //User was not signed in
+
+            }
+        }
+
+    }
+
+    public void signOut() {
+        AuthUI.getInstance().signOut(this);
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
         firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.signout) {
+            signOut();
+        }
     }
 }
